955595a9c07de2a958f5d487b236d8094ee582b0
abersheeran <me@abersheeran.com>
2024-10-22 11:17:21 +0800

    Add WebSocket API

--- src/fish_audio_sdk/__init__.py
+++ src/fish_audio_sdk/__init__.py
@@ -1,5 +1,14 @@
 from .apis import Session
 from .exceptions import HttpCodeErr
 from .schemas import ASRRequest, TTSRequest, ReferenceAudio
+from .websocket import WebSocketSession, AsyncWebSocketSession
 
-__all__ = ["Session", "HttpCodeErr", "ReferenceAudio", "TTSRequest", "ASRRequest"]
+__all__ = [
+    "Session",
+    "HttpCodeErr",
+    "ReferenceAudio",
+    "TTSRequest",
+    "ASRRequest",
+    "WebSocketSession",
+    "AsyncWebSocketSession",
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 
 import pytest
 
-from fish_audio_sdk.apis import Session
+from fish_audio_sdk import Session, WebSocketSession, AsyncWebSocketSession
 
 APIKEY = os.environ["APIKEY"]
 
@@ -10,3 +10,13 @@ APIKEY = os.environ["APIKEY"]
 @pytest.fixture
 def session():
     return Session(APIKEY)
+
+
+@pytest.fixture
+def sync_websocket():
+    return WebSocketSession(APIKEY)
+
+
+@pytest.fixture
+def async_websocket():
+    return AsyncWebSocketSession(APIKEY)
