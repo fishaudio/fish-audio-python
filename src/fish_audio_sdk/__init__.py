1cebbf5e48d36e5297f8c150abffc6e86f21c69b
Youngwook Kim <youngwook.kim@gmail.com>
2025-10-17 22:39:44 +0900

    feat: add WebSocketErr exception to the API

--- src/fish_audio_sdk/__init__.py
+++ src/fish_audio_sdk/__init__.py
@@ -1,11 +1,12 @@
 from .apis import Session
-from .exceptions import HttpCodeErr
+from .exceptions import HttpCodeErr, WebSocketErr
 from .schemas import ASRRequest, TTSRequest, ReferenceAudio, Prosody, PaginatedResponse, ModelEntity, APICreditEntity, StartEvent, TextEvent, CloseEvent
 from .websocket import WebSocketSession, AsyncWebSocketSession
 
 __all__ = [
     "Session",
     "HttpCodeErr",
+    "WebSocketErr",
     "ReferenceAudio",
     "TTSRequest",
     "ASRRequest",
